35540d66a23ec2d9c082ba9e50ce373f25e3959e
EthanArcher <ethanarcher1@gmail.com>
2024-12-01 11:41:51 +0000

    Day 1

--- Day01/main.go
+++ Day01/main.go
@@ -1,24 +1,86 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"math"
 	"os"
+	"sort"
+	"strconv"
 	"strings"
 )
 
 func main() {
-	fmt.Println(readCommaSeparatedFile("input.txt"))
-}
 
-func readCommaSeparatedFile(filename string) []string {
-	bs, err := os.ReadFile(filename)
+	lines := readLineSeparatedFile("input.txt")
+	leftList := []int{}
+	rightList := []int{}
 
-	if err != nil {
-		fmt.Println("Error: ", err)
-		os.Exit(1)
+	for _, line := range lines {
+		left, right, _ := strings.Cut(line, " ")
+		leftList = append(leftList, asANumber(left))
+		rightList = append(rightList, asANumber(right))
 	}
 
-	s := strings.Split(string(bs), ",")
+	sort.Ints(leftList)
+	sort.Ints(rightList)
 
-	return s
+	acc := 0
+
+	for i := range len(leftList) {
+		diff := leftList[i] - rightList[i]
+		acc += int(math.Abs(float64(diff))) 	
+	}
+
+	fmt.Println(acc)
+
+	sim := 0
+
+	for i := range len(leftList) {
+		sim += leftList[i] * countAppearances(leftList[i], rightList)
+	}
+	
+	fmt.Println(sim)
+
+}
+
+func countAppearances(n int, l []int) int {
+	count := 0
+	for _, num := range l {
+		if num == n {
+			count++
+		}
+	}
+	return count
+}
+
+func asANumber(s string) int {
+	num, err := strconv.Atoi(strings.TrimSpace(s))
+	if err != nil {
+		fmt.Errorf("error converting string '%s' to int: %w", s, err)	
+		return 0
+	}
+	return num
+}
+
+func readLineSeparatedFile(filename string) []string {
+	file, err := os.Open(filename)
+	if err != nil {
+		fmt.Errorf("error opening file: %w", err)
+		return nil
+	}
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Errorf("error reading file: %w", err)
+		return nil
+	}
+
+	return lines
 }
